7e0225890dbe3dd9abb74783502d2c921592eff9
Lucas <122111166+Lucas1792003@users.noreply.github.com>
2024-02-06 23:21:36 +0700

    FAQs feature finished

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -14,3 +14,11 @@ let arrow = document.querySelectorAll(".arrow");
             sidebarBtn.addEventListener("click",()=>{
                 sidebar.classList.toggle("close");
             });
+
+document.querySelector("#show-faq").addEventListener("click", function() {
+    document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click", function() {
+    document.querySelector(".popup").classList.remove("active");
+});
