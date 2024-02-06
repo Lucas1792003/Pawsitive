5a53474489a4062f6ba00401c3bf9fd65d7fff1d
Lucas <122111166+Lucas1792003@users.noreply.github.com>
2024-02-07 01:25:49 +0700

    Faq feauture
    Change design and fix bug and done 100% not gonna touch it again

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -22,3 +22,19 @@ document.querySelector("#show-faq").addEventListener("click", function() {
 document.querySelector(".popup .close-btn").addEventListener("click", function() {
     document.querySelector(".popup").classList.remove("active");
 });
+
+var acc = document.getElementsByClassName("question");
+var i;
+for(i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        this.parentElement.classList.toggle("active");
+        var answer = this.nextElementSibling; 
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+        
+    });
+}
