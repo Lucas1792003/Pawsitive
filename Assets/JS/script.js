7d7b41d15f78996800da806e50e081998464d1bb
Lucas <122111166+Lucas1792003@users.noreply.github.com>
2024-02-07 23:43:40 +0700

    search almost finished filter not done
    Working on the search page search is almost finished filter is not done

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -38,3 +38,19 @@ for(i = 0; i < acc.length; i++) {
         
     });
 }
+let searchBox = document.querySelector('#search-box');
+let images = document.querySelectorAll('.search-container .image-container .card .content .img');
+
+searchBox.oninput = () =>{
+   images.forEach(hide => hide.style.display = 'none');
+   let value = searchBox.value;
+   images.forEach(filter =>{
+      let title = filter.getAttribute('data-title');
+      if(value == title){
+         filter.style.display = 'block';
+      }
+      if(searchBox.value == ''){
+         filter.style.display = 'block';
+      }
+   });
+};
