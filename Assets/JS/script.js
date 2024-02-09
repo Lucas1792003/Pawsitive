6ffb6561d0017b6dfb2ce4c6cfa964f918087c5d
HarryYin03 <128616297+HarryYin03@users.noreply.github.com>
2024-02-10 00:53:14 +0700

    Add files via upload

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,22 @@
-let arrow = document.querySelectorAll(".arrow");
-            
-            for (var i = 0; i <arrow.length; i++){
-                arrow[i].addEventListener("click",(e)=>{
-                    
-                    let arrowParent = e.target.parentElement.parentElement;
-                    
-                    arrowParent.classList.toggle("showMenu");
-                });
-            }
-            let sidebar= document.querySelector(".sidebar");
-            let sidebarBtn= document.querySelector(".bx-menu");
-            console.log(sidebarBtn);
-            sidebarBtn.addEventListener("click",()=>{
-                sidebar.classList.toggle("close");
-            });
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+  
+  let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+      arrow[i].addEventListener("click", (e) => {
+          let arrowParent = e.target.parentElement.parentElement;
+          arrowParent.classList.toggle("showMenu");
+      });
+  }
+  
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  sidebarBtn.addEventListener("click", () => {
+      sidebar.classList.toggle("close");
+  });
+  
